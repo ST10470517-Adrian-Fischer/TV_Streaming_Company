43da6303815c8bb29447874a21f55eeaadb2e547
Adrian Anton Fischer <ST10470517@vcconnect.edu.za>
2025-08-03 13:04:12 +0200

    Update Series.java

--- src/main/java/com/mycompany/tvstreamingcompany/Series.java
+++ src/main/java/com/mycompany/tvstreamingcompany/Series.java
@@ -1,16 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template.
- */
+
 package com.mycompany.tvstreamingcompany;
 
 import java.util.ArrayList;
 import javax.swing.JOptionPane;
 
-/**
- *
- * @author adrianf
- */
+
 public class Series {
 
     private SeriesModel model;
